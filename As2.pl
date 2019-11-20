d81f9595a94d5777f279cdb423cfae18dbfab608
David <urdaibad@tcd.ie>
2019-11-20 00:20:47 +0000

    My implementation that fails.

--- As2.pl
+++ As2.pl
@@ -1,5 +1,5 @@
+%% Q1
 %% count starts at 0
-
 s --> u(0). 
 u(C) --> [2], {HCount is C/2}, v(HCount).  
 u(C) --> [0], {C1 is C+1}, u(C1).
@@ -8,6 +8,7 @@ v(0) --> [].
 v(C) --> [1], {CM1 is C-1}, v(CM1).
 v(C) --> [0], v(C).
 
+%% Q2
 s2 --> h(X,Y,Z), h(X2,Y2,Z2), {X2 \== X}, {Y2 \== Y}, {Z2 \== Z},
                      h(X3,Y3,Z3), {X3 \== X}, {Y3 \== Y}, {Z3 \== Z},
                        {X3 \== X2}, {Y3 \== Y2}, {Z3 \== Z2}.
@@ -24,4 +25,16 @@ lex(blue,     col).
 lex(green,    col).
 lex(zebra,    pet).
 lex(snail,    pet).
-lex(jaguar,   pet).      
+lex(jaguar,   pet).
+
+
+%% Q3
+sum(X) --> {mkList(X,[H|T])}, i(X,H).
+i(X,0)   --> [].
+i(X,Val) --> [Val], {XM is X - Val}, {XM == 0}, i(X,XM).
+i(X,Val) --> [Val], {XM is X - Val}, {XM > 0}, i(X,Val). 
+i(X,Val) --> i(X,ValM), {ValM is Val-1}. 
+
+%% mkList predicate
+mkList(0,[]).
+mkList(Num,[Num|List]) :- Num1 is Num-1, Num1 >= 0, mkList(Num1,List).
