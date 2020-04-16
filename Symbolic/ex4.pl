0fc98f4758028d3b89952e4803b43f73f56ee43f
davethedave41 <47148734+davethedave41@users.noreply.github.com>
2020-04-16 17:12:10 +0100

    Delete ex4.pl

--- Symbolic/ex4.pl
+++
@@ -1,9 +0,0 @@
- a2b([],[]).
- a2b([a|TA],[b|TB]) :- a2b(TA,TB). 
-
-combine1([],[],[]).
-combine1([H1|T1],[H2|T2], [wahooo,H1,H2|List]) :- combine1(T1,T2,List).
-
-member(X,[X|T]).
-member(X,[H|T]) :- member(X,T).
-
